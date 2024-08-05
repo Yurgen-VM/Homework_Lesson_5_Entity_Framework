91886b10ff28bf46b5eaa252b2782fa23a667b30
Yuriy <yuriy_vm@inbox.ru>
2024-08-05 07:42:56 +0300

    Добавьте файлы проекта.

---
+++ MessagePack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public enum Command
+    {
+        Register,
+        Message,
+        Confirmation,
+        List // Добавляем команду List, которую каждый клиент будет отправлять серверу при подключении, тем самым получать от сервера список непрочитанных сообщений.        
+    }
+    
+    // Класс для представления сетевого сообщения
+     
+    public class MessagePack
+    {
+        public Command Command { get; set; }
+        public int? Id { get; set; }
+        public string FromName { get; set; } // Имя отправителя
+        public string ToName { get; set; } // Имя получателя
+        public string Text { get; set; }
+        
+
+        // Метод для сериализации в JSON
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        // Статический метод для десериализации JSON в объект MyMessage
+
+        public static MessagePack FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<MessagePack>(json);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
